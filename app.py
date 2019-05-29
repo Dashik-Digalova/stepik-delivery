784da097ac6b21f78d05d610d32766668ddcf932
Dashik <d.digalova@yandex.ru>
2019-05-29 20:35:25 +0300

    new promotions added, promotions randomized

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
-
+import random
 app = Flask(__name__)
+
+
+stepik_alive = True
+
+workhours_opens = "10:00"
+workhours_closes = "22:00"
+
+promotion_text = "Сегодня скидка 15% по промокоду stepik"
+
+promocode = "stepik"
+
+promotions = [
+    "Сегодня скидка 15% по промокоду stepik",
+    "Сегодня скидка 10% по промокоду summer",
+    "Удваиваем все пиццы по промокоду udodopizza"
+]
 
 
 @app.route("/")
@@ -10,30 +26,26 @@ def hello():
 
 @app.route("/alive")
 def alive():
-    return '{"alive": true}'
+    if stepik_alive == True:
+        return '{"alive": true}'
+    else:
+        return '{"alive": false}'
 
 
 @app.route("/workhours")
 def workhours():
-    return '{"opens": "10:00", "closes": "22:00"}'
+    return '{"opens": "'+ workhours_opens +'", "closes": "'+ workhours_closes +'"}'
 
 
 @app.route("/promotion")
-def promotion_text():
-    return '{"promotion": "Сегодня скидка 15% по промокоду stepik"}'
-
-
-#@app.route("/promo/<stepik>")
-#def checkpromo(stepik):
-#    if stepik == 'stepik':
-#        return '{"valid":true}'
-#    else:
-#        return '{"valid":false}'
+def promotion():
+    promotion_number = random.randint(0, 2)
+    return '{"promotion": "'+promotions[promotion_number]+'"}'
 
 
 @app.route("/promo/<codes>")
 def checkpromo(codes):
-    if codes == 'stepik':
+    if codes == 'promocode':
         return '{"valid":true, "discount": 15}'
     elif codes == 'summer':
         return '{"valid":true, "discount": 10}'
